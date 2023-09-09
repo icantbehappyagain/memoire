63d2aec7656f2a5f4d80643d3b25498d1e9ca6b7
nouailmed <123259509+nouailmed@users.noreply.github.com>
2023-09-09 17:39:47 +0100

    Add files via upload

--- memoire.sql
+++ memoire.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 06, 2023 at 10:43 PM
--- Server version: 5.7.36
--- PHP Version: 7.4.26
+-- Host: localhost:3307
+-- Generation Time: Sep 09, 2023 at 04:34 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -27,13 +28,37 @@ SET time_zone = "+00:00";
 -- Table structure for table `order`
 --
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `datep` date NOT NULL,
-  PRIMARY KEY (`id`)
+  `datep` date NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `product` varchar(250) NOT NULL,
+  `client` varchar(50) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `total` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `product`, `client`, `phone`, `address`, `total`) VALUES
+(1, 'night box', 'ff', '5', 'f', '46870520 Da'),
+(2, 'night box', 'o8g', '8', 'iug', '46870520 Da'),
+(3, 'night box', 'o8g', '8', 'iug', '46870520 Da'),
+(4, 'egegrgrg', 'hpiuh', '9999', 'popok', '765299 Da'),
+(5, 'egegrgrg', 'ioh', '000', '[0u0u9', '1517512 Da');
 
 -- --------------------------------------------------------
 
@@ -41,12 +66,11 @@ CREATE TABLE IF NOT EXISTS `order` (
 -- Table structure for table `order_product`
 --
 
-DROP TABLE IF EXISTS `order_product`;
-CREATE TABLE IF NOT EXISTS `order_product` (
+CREATE TABLE `order_product` (
   `id_product` int(11) NOT NULL,
   `id_order` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -54,23 +78,30 @@ CREATE TABLE IF NOT EXISTS `order_product` (
 -- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `description` text NOT NULL,
-  `image_path` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `image_path` varchar(200) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `price`, `description`, `image_path`) VALUES
-(1, 'night box', 6543, 'box', 'img/2.jpg'),
-(2, 'Zahoui ', 45346465, '523325', 'img/7.jpg');
+(1, 'night box', 6543, 'box', 'pics/17.jpeg'),
+(2, 'Zahoui ', 45346465, '523325', 'pics/18.jpeg'),
+(3, 'egegrgrg', 758756, 'iobou', 'pics/19.jpeg'),
+(4, 'egegrgrg', 758756, 'iobou', 'pics/15.jpeg'),
+(5, 'egegrgrg', 758756, 'iobou', 'pics/16.jpeg'),
+(6, 'night box', 6543, 'box', 'pics/17.jpeg'),
+(7, 'Zahoui ', 45346465, '523325', 'pics/18.jpeg'),
+(8, 'egegrgrg', 758756, 'iobou', 'pics/19.jpeg'),
+(9, 'egegrgrg', 758756, 'iobou', 'pics/15.jpeg'),
+(10, 'egegrgrg', 758756, 'iobou', 'pics/16.jpeg'),
+(11, 'dove', 120, 'savone', 'pics/ibn-hayan-01.jpg');
 
 -- --------------------------------------------------------
 
@@ -78,16 +109,13 @@ INSERT INTO `product` (`id`, `name`, `price`, `description`, `image_path`) VALUE
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `fullname` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -96,6 +124,95 @@ CREATE TABLE IF NOT EXISTS `user` (
 INSERT INTO `user` (`id`, `fullname`, `email`, `password`, `is_admin`) VALUES
 (1, 'admin', 'admin@gmail.com', 'admin', 1),
 (2, 'aya', 'aya15zahoui@gmail.com', '1234567890', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(1, 'mohamed@gmail.com', 'moh'),
+(2, 'nouailmed@gmail.com', 'ey');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
